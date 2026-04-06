6d5e07b5b8c039af6d3125d639d43d3001dbd295
knarta <charquen10@gmail.com>
2026-04-06 09:20:17 +0200

    Update docker-compose to change volume path and refactor index.js for improved readability; remove outdated content from README.md

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const sequelize = require('./config/database');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRouter = require('./routes/auth')
-const titlesRouter = require('./routes/titles')
-const userActivitiesRouter = require('./routes/userActivities')
-const fs = require("fs");
-require('dotenv').config()
+const authRouter = require('./routes/auth');
+const titlesRouter = require('./routes/titles');
+const userActivitiesRouter = require('./routes/userActivities');
+const fs = require('fs').promises;
+require('dotenv').config();
 
 const app = express();
 const corsOptions = {
@@ -21,16 +21,16 @@ app.use('/api/auth', authRouter);
 app.use('/api/titles', titlesRouter);
 app.use('/api/activity', userActivitiesRouter);
 
-sequelize.sync({ force: true })
+sequelize
+    .sync({ force: true })
     .then(async () => {
-        console.log(`Database & tables created!`);
-        console.log('Postgress Connected');
-        fs.readFile("dump.sql", 'utf8', async (err, data) => {
-            await sequelize.query(data)
-            console.log("DB Seeded");
-        })
+        console.log('Database & tables created!');
+        console.log('PostgreSQL connected');
+        const data = await fs.readFile('dump.sql', 'utf8');
+        await sequelize.query(data);
+        console.log('DB Seeded');
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 
 const port = process.env.PORT || 8000;
 
